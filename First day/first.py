eb8623f5dd2132f1b496a9964e8a351795c29e86
Aryan Patel <patelaryan232004@gmail.com>
2025-06-02 20:28:40 +0530

    Add files via upload

---
+++ First day/first.py
@@ -0,0 +1,28 @@
+##r = 12
+##print("R",r)
+#a=10
+#b=5
+##
+##a=(int(input("Enter the value of a:")))
+##b=(int(input("Enter the value of b:")))
+##print("Value of a & b is:",a,b)
+##print("Addition of a & b is:",a+b)
+##print("Subtration of a & b is:",a-b)
+##print("Multiplication of a & b is:",a*b)
+##print("Division of a & b is:",a/b)
+##print("Module of a & b is:",a%b)
+##
+##c=(input("Enter value of c : "))
+##print("value of c :",c)
+##
+##
+##x=int(input("Enter any number:"))
+##print("Number value:",x)
+##y=(input("Enter any string:"))
+##print("String value:",y)
+##p=float(input("Enter any float number:"))
+##print("Float number:",p)
+
+a=int(input("Enter a value:"))
+print("Value is entered:",a)
+
