a3021221e679380ef987eb928acc1ea88643f74d
Aryan Patel <patelaryan232004@gmail.com>
2025-06-04 12:12:29 +0530

    Add files via upload

---
+++ Fourth day/forloop.py
@@ -0,0 +1,28 @@
+#For loop
+##
+##for i in range(5):
+##    print(i)
+
+##a = [1,2,3]
+##for i in a:
+##    print(i)
+##
+
+
+#Table of number using input
+
+##number = int(input("Enter a number:"))
+##
+##for i in range(11):
+##    if i == 0:
+##        continue
+##    result = i * number
+##    print(number,"*",i,"=",result)
+##    
+
+
+
+string = input("Enter a string:")
+
+for i in string:
+    print(i)
---
+++ Fourth day/whileloop.py
@@ -0,0 +1,27 @@
+#while loop
+
+##j = 1
+##while j < 5:
+##    print(j)
+##    j += 1
+
+
+##number = int(input("Enter a number:"))
+##
+##i = 1
+##while i <= 10 :
+##    result = number * i
+##    print(number,"*",i,"=",result)
+##    i = i+1
+
+# Sum of 1 to 10 numbers
+
+sum1 = 1
+i = 1
+while i < 10:
+    i += 1
+    sum1 = sum1+i
+    #print("sum:",sum1,"i",i)
+print("Sum is:",sum1)
+
+    
