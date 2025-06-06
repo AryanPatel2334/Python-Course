64500768f13d2da52e3e9af5269aebec9e88572a
Aryan Patel <patelaryan232004@gmail.com>
2025-06-06 19:57:52 +0530

    Add files via upload

---
+++ 6th day/arraydatatype.py
@@ -0,0 +1,38 @@
+##Array Datatypes
+
+#1. List
+
+list1 = ["Apple","Mango","Banana"]
+print(list1)
+
+#accessing items of list
+
+##print(list1[0],list1[1],list1[2])
+
+for i in list1:
+    print(i)
+print(type(list1))
+
+#2. Set
+
+set1 = {"Groww","AngelOne","Zerodha"}
+print(set1)
+
+print(type(set1))
+
+#3. Tuple
+
+tuple1 = ("Keshav","Madhav","Govinda")
+print(tuple1)
+print(type(tuple1))
+
+#4. Dictionary
+
+dic1 = {1:"Aryan",
+        2:"Keval",
+        3:"Jitesh",
+        4:"Abhishek"}
+
+print(dic1)
+print(type(dic1))
+
---
+++ 6th day/typeconversion.py
@@ -0,0 +1,25 @@
+#Type conversion
+
+#Float to integer
+
+##a = 12.5
+##print(type(a))
+##b= int(a)
+##print(b)
+##print(type(b))
+
+##value = float(input("Enter a float number:"))
+##print("Float to integer:",int(value))
+
+#integer to float
+
+##a = 55
+##print(type(a))
+##b = float(a)
+##print(float(b))
+##print(type(b))
+value = int(input("Enter a number:"))
+print("Float to integer:",float(value))
+
+
+
